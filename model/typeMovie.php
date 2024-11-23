cbce89ab9ac9713cb070459283f2c00cc32315f2
ThuyTrang9525 <buithithuytrang0905@gmail.com>
2024-11-23 23:38:37 +0700

    change idea

--- model/typeMovie.php
+++ model/typeMovie.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" href="../assets/css/style.css">
 <h2>Top 5 phim hôm nay</h2>
     <div class="top_phim">
-        <a href="#"><img src="/img/images1932699_19R.jpg" alt="Movie Suggestion 1"></a>
-        <a href="#"><img src="/img/images1932699_19R.jpg" alt="Movie Suggestion 3"></a>
-        <a href="#"><img src="/img/images1932699_19R.jpg" alt="Movie Suggestion 4"></a>
-        <a href="#"><img src="/img/images1932699_19R.jpg" alt="Movie Suggestion 5"></a>
-        <a href="#"><img src="/img/images1932699_19R.jpg" alt="Movie Suggestion 6"></a>
-        <a href="#"><img src="/img/images1932699_19R.jpg" alt="Movie Suggestion 7"></a>
+        <a href="#"><img src="https://phimimg.com/upload/vod/20241104-1/26e9a849cc1c17ead82dadbbca3d62a5.jpg" alt="Movie Suggestion 1"></a>
+        <a href="#"><img src="https://phimimg.com/upload/vod/20241104-1/26e9a849cc1c17ead82dadbbca3d62a5.jpg" alt="Movie Suggestion 2"></a>
+        <a href="#"><img src="https://phimimg.com/upload/vod/20241104-1/26e9a849cc1c17ead82dadbbca3d62a5.jpg" alt="Movie Suggestion 3"></a>
+        <a href="#"><img src="https://phimimg.com/upload/vod/20241104-1/26e9a849cc1c17ead82dadbbca3d62a5.jpg" alt="Movie Suggestion 4"></a>
+        <a href="#"><img src="https://phimimg.com/upload/vod/20241104-1/26e9a849cc1c17ead82dadbbca3d62a5.jpg" alt="Movie Suggestion 5"></a>
+
     </div>
 
     <h2>Phim thuê đặc sắc</h2>
---
+++ model/useMore.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="../assets/css/style.css">
+<div class="banner">
+    <img src="../assets/image/Hình ảnh/banner con 1.jpg" />
+</div>
+<h2>Phim thuê đặc sắc</h2>
+    <div class="phim_thue">
+        <a href="#"><img src="https://storage.googleapis.com/a1aa/image/v1a41eJBVg1OLCfntsbZJUNSlrmlnsefoyv0744WLjy9sPOPB.jpg" alt="Movie Suggestion 1"></a>
+        <a href="#"><img src="https://storage.googleapis.com/a1aa/image/v1a41eJBVg1OLCfntsbZJUNSlrmlnsefoyv0744WLjy9sPOPB.jpg" alt="Movie Suggestion 2"></a>
+        <a href="#"><img src="https://storage.googleapis.com/a1aa/image/v1a41eJBVg1OLCfntsbZJUNSlrmlnsefoyv0744WLjy9sPOPB.jpg" alt="Movie Suggestion 3"></a>
+        <a href="#"><img src="https://storage.googleapis.com/a1aa/image/v1a41eJBVg1OLCfntsbZJUNSlrmlnsefoyv0744WLjy9sPOPB.jpg" alt="Movie Suggestion 4"></a>
+        <a href="#"><img src="https://storage.googleapis.com/a1aa/image/v1a41eJBVg1OLCfntsbZJUNSlrmlnsefoyv0744WLjy9sPOPB.jpg" alt="Movie Suggestion 5"></a>
+    </div>
+<div class="banner">
+    <img alt="Banner for The Deal series" src="../assets/image/bannercon2.jpg" />
+</div>
+<div class="section-title">Phim sắp chiếu</div>
+    <div class="upcoming-movies">
+        <div class="movie">
+            <img alt="Movie poster of upcoming movie 1" src="https://storage.googleapis.com/a1aa/image/v1a41eJBVg1OLCfntsbZJUNSlrmlnsefoyv0744WLjy9sPOPB.jpg" />
+        </div>
+        <div class="movie">
+            <img alt="Movie poster of upcoming movie 2" src="https://storage.googleapis.com/a1aa/image/fOVfTnSLmtuEDkBfuJ7i7pTzq3qzvdCXSsbx4fsmz0RTsPOPB.jpg" />
+        </div>
+        <div class="movie">
+            <img alt="Movie poster of upcoming movie 3" src="https://storage.googleapis.com/a1aa/image/lc9xOQmulf0qTqiOqSPi2pg7WsjV32JVHFjWPQrTlvfS7jzTA.jpg" />
+        </div>
+        <div class="movie">
+            <img alt="Movie poster of upcoming movie 4" src="https://storage.googleapis.com/a1aa/image/oePfzFTNXThkd0ujFfCk4QW6yxQIopQjGrM6HFhBaQFE2HnnA.jpg" />
+        </div>
+        <div class="movie">
+            <img alt="Movie poster of upcoming movie 5" src="https://storage.googleapis.com/a1aa/image/JqekJTf5O5kqokJsPn53ZweMfwXA5K3yqqtJO7NDsbgYsPOPB.jpg" />
+        </div>
+    </div>
+</div>
