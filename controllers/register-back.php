b555a19c18ad443fa7af6e794f90e84835bc2dd9
ThiKim148 <hothikim14102004@gmail.com>
2024-11-26 10:15:41 +0700

    done 26/11

---
+++ controllers/register-back.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    error_reporting(E_ALL ^ E_DEPRECATED);
+    require_once '../model/connect.php';
+
+    if (isset($_POST['sign up']))
+    {
+        if (isset($_POST['username'])) {
+            $username = $_POST['username'];
+        }
+
+        if (isset($_POST['password'])) {
+            $password = $_POST['password'];
+        }
+
+        if (isset($_POST['email'])) {
+            $email = $_POST['email'];
+        }
+
+        // if (isset($_POST['phone'])) {
+        //     $phone = $_POST['phone'];
+        // }
+
+        $sql = "INSERT INTO users ( username, password, email, role)
+                VALUES ('$username', md5('$password'), '$email', 1)";
+        $res = mysqli_query($conn,$sql);
+        if ($res) 
+        {
+            header("location:login.php?rs=success");
+            exit();
+        }
+        else 
+        {
+            header("location:login.php?rf=fail");
+            exit();
+        }
+    }
+?>
--- model/voucher.php
+++ model/voucher.php
@@ -15,17 +15,17 @@
         </p>
         <div class="bigbox">
             <div class="box-discount">
-                <h2 class="discount-title">Mã giảm giá</h2>
+                <h2 class="discount-title" style="color: black;">Mã giảm giá</h2>
                 <div class="all-discount">
                     <div class="voucher">
                         <div class="discount">
                             <div class="box-logo">
-                                <img class="logo" src="./Hình ảnh/logo.png">
+                                <img class="logo" src="../assets/image/Hình ảnh/logo.png">
                             </div>
                             <div class="detail-discount">
                                  <div class="text-voucher">
                                     <p>17-11-2024 đến 17-12-2024 </p>
-                                    <p style="color:red"><b>Giảm ngay 5% cho hóa đơn trên 200k!</b></p>
+                                    <p style="color:red;"><b>Giảm ngay 5% cho hóa đơn trên 200k!</b></p>
                                     <p>Chỉ áp dụng cho 1 số phim nhất định </p>
                                     <span class="fa fa-star checked"></span>
                                     <span class="fa fa-star checked"></span>
@@ -41,52 +41,7 @@
                         </div>
                         <div class="discount">
                             <div class="box-logo">
-                                <img class="logo" src="./Hình ảnh/logo.png">
-                            </div>
-                            <div class="detail-discount">
-                                 <div class="text-voucher">
-                                    <p>17-11-2024 đến 17-12-2024 </p>
-                                    <p style="color:red"><b>Giảm ngay 5% cho hóa đơn trên 200k!</b></p>
-                                    <p>Chỉ áp dụng cho 1 số phim nhất định </p>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star"></span>
-                                    <span class="fa fa-star"></span>
-                                 </div>
-                                 <div class="condition">
-                                    <p>Điều kiện</p>
-                                    <p style="color:red">Dùng ngay</p>
-                                 </div>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="voucher">
-                        <div class="discount">
-                            <div class="box-logo">
-                                <img class="logo" src="./Hình ảnh/logo.png">
-                            </div>
-                            <div class="detail-discount">
-                                 <div class="text-voucher">
-                                    <p>17-11-2024 đến 17-12-2024 </p>
-                                    <p style="color:red"><b>Giảm ngay 5% cho hóa đơn trên 200k!</b></p>
-                                    <p>Chỉ áp dụng cho 1 số phim nhất định </p>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star checked"></span>
-                                    <span class="fa fa-star"></span>
-                                    <span class="fa fa-star"></span>
-                                 </div>
-                                 <div class="condition">
-                                    <p>Điều kiện</p>
-                                    <p style="color:red">Dùng ngay</p>
-                                 </div>
-                            </div>
-                        </div>
-                        <div class="discount">
-                            <div class="box-logo">
-                                <img class="logo" src="./Hình ảnh/logo.png">
+                                <img class="logo" src="../assets/image/Hình ảnh/logo.png">
                             </div>
                             <div class="detail-discount">
                                  <div class="text-voucher">
@@ -110,7 +65,7 @@
                     <div class="voucher">
                         <div class="discount">
                             <div class="box-logo">
-                                <img class="logo" src="./Hình ảnh/logo.png">
+                                <img class="logo" src="../assets/image/Hình ảnh/logo.png">
                             </div>
                             <div class="detail-discount">
                                  <div class="text-voucher">
@@ -131,7 +86,52 @@
                         </div>
                         <div class="discount">
                             <div class="box-logo">
-                                <img class="logo" src="./Hình ảnh/logo.png">
+                                <img class="logo" src="../assets/image/Hình ảnh/logo.png">
+                            </div>
+                            <div class="detail-discount">
+                                 <div class="text-voucher">
+                                    <p>17-11-2024 đến 17-12-2024 </p>
+                                    <p style="color:red"><b>Giảm ngay 5% cho hóa đơn trên 200k!</b></p>
+                                    <p>Chỉ áp dụng cho 1 số phim nhất định </p>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                 </div>
+                                 <div class="condition">
+                                    <p>Điều kiện</p>
+                                    <p style="color:red">Dùng ngay</p>
+                                 </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="voucher">
+                        <div class="discount">
+                            <div class="box-logo">
+                                <img class="logo" src="../assets/image/Hình ảnh/logo.png">
+                            </div>
+                            <div class="detail-discount">
+                                 <div class="text-voucher">
+                                    <p>17-11-2024 đến 17-12-2024 </p>
+                                    <p style="color:red"><b>Giảm ngay 5% cho hóa đơn trên 200k!</b></p>
+                                    <p>Chỉ áp dụng cho 1 số phim nhất định </p>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>
+                                 </div>
+                                 <div class="condition">
+                                    <p>Điều kiện</p>
+                                    <p style="color:red">Dùng ngay</p>
+                                 </div>
+                            </div>
+                        </div>
+                        <div class="discount">
+                            <div class="box-logo">
+                                <img class="logo" src="../assets/image/Hình ảnh/logo.png">
                             </div>
                             <div class="detail-discount">
                                  <div class="text-voucher">
@@ -154,32 +154,32 @@
                 </div>
             </div>
             <div class="box-recomment">
-                <h2 class="rcm"> Các phim tương tự</h2>
+                <h2 style="color: black;" class="rcm"> Các phim tương tự</h2>
                 <div class="img-rcm">
-                    <img src="./Hình ảnh/image 31.jpg">
-                    <img src="./Hình ảnh/image 32.jpg">
-                    <img src="./Hình ảnh/image 29.jpg">
+                    <img src="../assets/image/Hình ảnh/image 31.jpg">
+                    <img src="../assets/image/Hình ảnh/image 32.jpg">
+                    <img src="../assets/image/Hình ảnh/image 29.jpg">
                 </div>
             </div>
 
         </div>
-        <img class="banner" src="./Hình ảnh/Banner con 1.jpg">
-        <h2 class="film-rental">Phim thuê đặc sắc</h2>
-        <div class="recomment-film">
-            <img class="img-recomment" src="./Hình ảnh/image 6.jpg">
-            <img class="img-recomment" src="./Hình ảnh/image 7.jpg">
-            <img class="img-recomment" src="./Hình ảnh/chichiemem 1.jpg">
-            <img class="img-recomment" src="./Hình ảnh/chichiemem 2.jpg">
-            <img class="img-recomment" src="./Hình ảnh/chichiemem 3.jpg">
+        <img style="width: 100%;" class="banner-voucher" src="../assets/image/Hình ảnh/Banner con 1.jpg">
+        <h2 style="color: black;"  class="film-rental">Phim thuê đặc sắc</h2>
+        <div class="recomment-film" style="width: 100%; gap: 10px; justify-content: space-evenly; padding: 0;">
+            <img class="img-recomment" src="../assets/image/Hình ảnh/image 6.jpg">
+            <img class="img-recomment" src="../assets/image/Hình ảnh/image 7.jpg">
+            <img class="img-recomment" src="../assets/image/Hình ảnh/chichiemem 1.jpg">
+            <img class="img-recomment" src="../assets/image/Hình ảnh/chichiemem 2.jpg">
+            <img class="img-recomment" src="../assets/image/Hình ảnh/chichiemem 3.jpg">
         </div>
-        <img class="banner" src="./Hình ảnh/Banner con 3.jpg">
-        <h2 class="film-upcomming">Phim sắp chiếu</h2>
-        <div class="box-comming-film">
-            <img class="img-comming" src="./Hình ảnh/image 51.jpg">
-            <img class="img-comming" src="./Hình ảnh/image 52.jpg">
-            <img class="img-comming" src="Hình ảnh/image 49.jpg">
-            <img class="img-comming" src="Hình ảnh/image 50.jpg">
-            <img class="img-comming" src="Hình ảnh/image 53.jpg">
+        <img style="margin-right: 10px; width: 100%;" class="banner" src="../assets/image/Hình ảnh/Banner con 3.jpg">
+        <h2 style="color: black;" class="film-upcomming">Phim sắp chiếu</h2>
+        <div class="box-comming-film" style="gap: 10px; justify-content: space-evenly; padding: 0;">
+            <img class="img-comming" src="../assets/image/Hình ảnh/image 51.jpg">
+            <img class="img-comming" src="../assets/image/Hình ảnh/image 52.jpg">
+            <img class="img-comming" src="../assets/image/Hình ảnh/image 49.jpg">
+            <img class="img-comming" src="../assets/image/Hình ảnh/image 50.jpg">
+            <img class="img-comming" src="../assets/image/Hình ảnh/image 53.jpg">
         </div>
 
    </div>
